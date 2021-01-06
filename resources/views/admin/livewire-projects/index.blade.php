466f8a39cffbf36a9d5682c4b509da96e193b066
David Lun <mysticcode@gmail.com>
2021-01-06 03:04:07 +0200

    fix css mess

--- resources/views/admin/livewire-projects/index.blade.php
+++ resources/views/admin/livewire-projects/index.blade.php
@@ -2,20 +2,21 @@
 @section('content')
     <div class="card">
         <div class="card-header">
-            <div class="card-row">
-                <h6 class="card-title">{{ trans('cruds.project.title_singular') }} {{ trans('global.list') }}</h6>
+            <div class="card-header-container">
+                <h3 class="card-title">
+                    {{ trans('cruds.project.title_singular') }}
+                    {{ trans('global.list') }}
+                </h3>
+
                 @can('project_create')
-                    <a class="card-button" href="{{ route('admin.livewire-projects.create') }}">
+                    <a class="btn btn-primary" href="{{ route('admin.livewire-projects.create') }}">
                         {{ trans('global.add') }} {{ trans('cruds.project.title_singular') }}
                     </a>
                 @endcan
             </div>
         </div>
 
-        <div class="card-body">
-            <div class="table-responsive">
-                @livewire('projects.index')
-            </div>
-        </div>
+        @livewire('projects.index')
+
     </div>
 @endsection
--- resources/views/livewire/projects/index.blade.php
+++ resources/views/livewire/projects/index.blade.php
@@ -1,30 +1,30 @@
 <div>
-    <div class="row mb-3 sm:flex">
+    <div class="card-body sm:flex">
         <div class="w-full sm:w-1/2">
             Per page:
-            <select wire:model="entriesPerPage" class="form-select w-full sm:w-1/6">
-                <option>10</option>
-                <option>25</option>
-                <option>50</option>
-                <option>100</option>
+            <select wire:model="perPage" class="form-select w-full sm:w-1/6">
+                @foreach($paginationOptions as $value)
+                    <option value="{{ $value }}" wire:click="resetPage()">{{ $value }}</option>
+                @endforeach
             </select>
 
             @if (count($selectedEntries) > 0)
                 <button wire:click="deleteSelected"
                         class="btn btn-sm btn-danger ml-3"
-                        onclick="return confirm('Are you sure?') || event.stopImmediatePropagation()">Delete Selected
+                        onclick="return confirm('Are you sure?') || event.stopImmediatePropagation()">
+                        Delete Selected
                 </button>
             @endif
         </div>
         <div class="w-full sm:w-1/2 sm:text-right">
             Search:
-            <input type="text" wire:model.debounce.500ms="searchQuery" class="w-full sm:w-1/3 inline-block"/>
+            <input type="text" wire:model.debounce.300ms="search" class="w-full sm:w-1/3 inline-block"/>
         </div>
     </div>
     <div wire:loading.delay class="col-12 alert alert-info">
         Loading...
     </div>
-    <table class="table table-striped">
+    <table class="table">
         <thead>
         <tr>
             <th></th>
@@ -98,5 +98,9 @@
         </tbody>
     </table>
 
-    {{ $projects->links() }}
+    <div>
+    <div class="card-body">
+        {{ $projects->links() }}
+    </div>
+</div>
 </div>
